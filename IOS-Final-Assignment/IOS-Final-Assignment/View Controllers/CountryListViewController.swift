ea8a6b31f04cd19c7b533477888bb7ec3a0f0943
samm-w <sammw98@gmail.com>
2021-03-22 10:53:22 -0700

    Created view controllers and storyboard screen layout

---
+++ IOS-Final-Assignment/IOS-Final-Assignment/View Controllers/CountryListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CountryListViewController.swift
+//  IOS-Final-Assignment
+//
+//  Created by  on 3/22/21.
+//
+
+import UIKit
+
+class CountryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
