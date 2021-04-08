d50bbb19f38fb74de8406f5eb9a863b6a50d26f3
samm-w <sammw98@gmail.com>
2021-04-08 12:32:54 -0700

    Added audio playing to homepage

--- IOS-Final-Assignment/IOS-Final-Assignment/View Controllers/ViewController.swift
+++ IOS-Final-Assignment/IOS-Final-Assignment/View Controllers/ViewController.swift
@@ -6,8 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet var volSlider: UISlider!
+    var soundPlayer: AVAudioPlayer!
+    
+    @IBAction func volumeDidChange(sender: UISlider){
+        soundPlayer?.volume = volSlider.value
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let soundURL = Bundle.main.path(forResource: "song", ofType: "mp3")!
+        let url = URL(fileURLWithPath: soundURL)
+        self.soundPlayer = try! AVAudioPlayer.init(contentsOf: url)
+        self.soundPlayer.currentTime = 0
+        self.soundPlayer.volume = volSlider.value
+        self.soundPlayer.numberOfLoops = -1
+        self.soundPlayer.play()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.soundPlayer.stop()
+    }
     
     @IBAction func unwindToHomeVC(sender: UIStoryboardSegue!){}
 
