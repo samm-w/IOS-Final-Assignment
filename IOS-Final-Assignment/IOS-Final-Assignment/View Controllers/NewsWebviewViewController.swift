b62019b5303208ce1aaec6814ef5e704a62405b9
Edmund Leung <edmundleungsa@gmail.com>
2021-04-17 11:38:09 -0700

    Add comments to the article classes

--- IOS-Final-Assignment/IOS-Final-Assignment/View Controllers/NewsWebviewViewController.swift
+++ IOS-Final-Assignment/IOS-Final-Assignment/View Controllers/NewsWebviewViewController.swift
@@ -1,9 +1,9 @@
 //
 //  NewsWebviewViewController.swift
 //  IOS-Final-Assignment
-//
+//  Prinicpal Author: Edmund Leung
+//  Description: A class to load the website that is linked to the specific article
 //  Created by  on 4/17/21.
-//
 
 import UIKit
 import WebKit
@@ -17,6 +17,7 @@ class NewsWebviewViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Load the URL webpage of the article that is selected
         webView.load(URLRequest(url: URL(string: url!)!))
     }
 
